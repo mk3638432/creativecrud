e58434b4b656f2c3fdc6fed113cd5c6b1db85443
mk3638432 <mk3638432@gmail.com>
2023-12-04 14:00:26 +0530

    project initalize

---
+++ components/EditTopicform.jsx
@@ -0,0 +1,122 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+
+const EditTopicform = ({ topic }) => {
+  const router = useRouter();
+  const { _id, name, type, email, phone, alternatPhone } = topic;
+
+  const [newname, setNewName] = useState(name);
+  const [newtype, setNewType] = useState(type);
+  const [newemail, setNewEmail] = useState(email);
+  const [newphone, setNewPhone] = useState(phone);
+  const [newalternatPhone, setNewAlternatPhone] = useState(alternatPhone);
+  const options = ["PHP", "Node-Js", "React-Js"];
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`/api/topics/${_id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          newname,
+          newtype,
+          newemail,
+          newphone,
+          newalternatPhone,
+        }),
+      });
+      toast.success("Edited successfully");
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <div className="flex justify-center items-center  ">
+        <form>
+          <div className="mt-5">
+            <div className="flex gap-[1vw] flex-col	 ">
+              <p> Name:</p>
+              <input
+                className="border border-[#1B1B1B]  w-[45vw] rounded-lg p-3 "
+                type="text"
+                name="name"
+                value={newname}
+                onChange={(e) => setNewName(e.target.value)}
+                placeholder="Enter Your First Name"
+              />
+            </div>
+
+            <div className="mt-3 flex ">
+              <div className="flex gap-[1vw] flex-col	 ">
+                <p>Email id : </p>
+                <input
+                  className="border border-[#1B1B1B]  w-[45vw] rounded-lg p-3 "
+                  type="email"
+                  name="email"
+                  value={newemail}
+                  onChange={(e) => setNewEmail(e.target.value)}
+                  placeholder="Enter Your Last Name"
+                />
+              </div>
+            </div>
+            <div className="flex gap-[1vw] flex-col	 ">
+              <p> Type:</p>
+            </div>
+            <select
+              className="border border-[#1B1B1B] w-[45vw] rounded-lg p-3"
+              value={newtype}
+              name="type"
+              onChange={(e) => setNewType(e.target.value)}
+            >
+              {options.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            <div className="flex gap-[1vw] flex-col	 ">
+              <p> Mobile_no :</p>
+              <input
+                className="border border-[#1B1B1B]  w-[45vw] rounded-lg p-3 "
+                type="number"
+                name="phone"
+                value={newphone}
+                onChange={(e) => setNewPhone(e.target.value)}
+                placeholder="Enter Your ester_mobile_no"
+              />
+            </div>
+            <div className="flex gap-[1vw] flex-col	 ">
+              <p> Alternative Mobile_no:</p>
+              <input
+                className="border border-[#1B1B1B]  w-[45vw] rounded-lg p-3 "
+                type="number"
+                name="alternatPhone"
+                value={newalternatPhone}
+                onChange={(e) => setNewAlternatPhone(e.target.value)}
+                placeholder="Enter Your Alternative No.:"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className=" flex justify-center">
+        <button
+          onClick={handleUpdate}
+          className="bg-[#1B1B1B]  w-[20vw] mt-10 text-white font-bold py-2 px-4 "
+        >
+          Update
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default EditTopicform;
---
+++ components/TopicList.jsx
@@ -0,0 +1,114 @@
+"use client";
+import React, { useEffect } from "react";
+import Removebtn from "./Removebtn";
+import Link from "next/link";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import axios from "axios";
+import moment from "moment";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3001/api/topics", {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+    return res.json();
+  } catch (error) {}
+};
+const TopicList = async () => {
+  const { topics } = await getTopics();
+
+  return (
+    <>
+      <div className="my-10  ">
+        {/* <h2 className=" text-2xl "> Php_test_mast </h2> */}
+        <table className=" mx-auto text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs mx-10  h-10 text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th className="px-6 py-3">ID</th>
+              <th className="px-6 py-3">Name</th>
+              <th className="px-6 py-3">Type</th>
+              <th className="px-6 py-3"> Email</th>
+              <th className="px-6 py-3">Mobile No</th>
+              <th className="px-6 py-3">Alternative No</th>
+              <th className="px-6 py-3">Creation_date </th>
+              <th className="px-6 py-3">last_updation_date </th>
+              <th> </th>
+              <th> </th>
+            </tr>
+          </thead>
+          <tbody className="mt-10">
+            {topics.map((test, index) => (
+              <TestRow
+                getTopics={getTopics}
+                key={test?._id}
+                index={index}
+                test={test}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default TopicList;
+
+export const TestRow = ({ getTopics, test, index }) => {
+  const router = useRouter();
+  const handleDelete = async (id) => {
+    try {
+      const response = await axios.delete(`/api/topics/?id=${id}`);
+      toast.success(" topics deleted successfully");
+      if (response.ok) {
+        // router.refresh();
+      }
+    } catch (error) {
+      toast.error("Error deleting user:");
+    }
+  };
+
+  return (
+    <tr
+      key={test.test_id}
+      className={`text-xs mx-10 h-10 text-gray-700 uppercase ${
+        test.type === "PHP"
+          ? "bg-green-400"
+          : test.type === "Node-Js"
+          ? "bg-yellow-400"
+          : "bg-orange-400"
+      }`}
+    >
+      <td className="px-6 py-3">{index + 1}</td>
+      <td className="px-6 py-3">{test?.name}</td>
+      <td className="px-6 py-3">{test?.type}</td>
+      <td className="px-6 py-3">{test?.email}</td>
+      <td className="px-6 py-3">{test?.phone}</td>
+      <td className="px-6 py-3">{test?.alternatPhone}</td>
+      <td className="px-6 py-3">
+        {" "}
+        {moment(test?.createdAt).format("YYYY-MM-DD HH:mm:ss")}
+      </td>
+      <td className="px-6 py-3">
+        {" "}
+        {moment(test?.updatedAt).format("YYYY-MM-DD HH:mm:ss")}{" "}
+      </td>
+      <td>
+        <Link href={`editTopic/${test?._id}`}>
+          <FaEdit size={20} />
+        </Link>
+      </td>
+      <td>
+        <button onClick={() => handleDelete(test?._id)}>
+          <MdDelete color="red" size={20} />
+        </button>
+      </td>
+    </tr>
+  );
+};
